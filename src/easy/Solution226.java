37f85668872098bf4f5d39a7ce8d164f17f1595d
unknown <>
2017-08-07 19:31:16 +0800

    update

--- src/easy/Solution226.java
+++ src/easy/Solution226.java
@@ -4,20 +4,34 @@ package easy;
  * Created by lenovo on 2017/8/7.
  */
 public class Solution226 {
-    class TreeNode {
+    static class TreeNode {
       int val;
       TreeNode left;
       TreeNode right;
       TreeNode(int x) { val = x; }
     }
 
-    public TreeNode invertTree(TreeNode root) {
+    public static TreeNode invertTree(TreeNode root) {
         if(root == null) {
             return root;
         }
-        TreeNode t = root;
-        root.left = invertTree(t.right);
-        root.right = invertTree(t.left);
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = invertTree(right);
+        root.right = invertTree(left);
         return root;
+    }
+
+    public static void main(String args[]){
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+
+        invertTree(root);
+
     }
 }
